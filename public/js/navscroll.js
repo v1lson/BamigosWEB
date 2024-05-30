908aff988b9ff5e5fd34a0fc460e144d883c2305
v1lson <andreystavenko26@gmail.com>
2024-05-30 10:55:55 +0000

    pequeño cambio

--- public/js/navscroll.js
+++ public/js/navscroll.js
@@ -2,16 +2,15 @@
     if ($(this).scrollTop() === 0) {
     $('navbar').css('top', '70px');
     $('navbar').css('height', '');
-        $('navbar ul').css('margin-top', 'auto');
+    $('navbar ul').css('margin-top', 'auto');
+    $('navbar #menu').css('margin-bottom', ''); 
 } else {
     $('navbar').css('top', '0px');
     $('navbar').css('height', '100vh');
-    $('navbar #menu').css('margin-top', '20vh');
+    $('navbar #menu').css('margin-top', '30vh');
+    $('navbar #menu').css('margin-bottom', 'auto');
     $('main').css('padding-bottom', '70px');
 }
 });
 
-function test(){
-    console.log('Bro funciona')
-}
 
